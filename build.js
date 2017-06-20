1eea839fd4070e4c05cea7e2df6eacb41999cc05
gabe <gabriel.debeaupuis@gmail.com>
2017-06-19 17:32:59 -0700

    deploy-test

--- build.js
+++ build.js
@@ -6,7 +6,7 @@ var consoleLog = false; // set true for metalsmith file and meta content logging
 var devBuild = ((process.env.NODE_ENV || '').trim().toLowerCase() !== 'production');
 var pkg = require('./package.json');
 
-  // main directories
+// main directories
 var dir = {
   base: __dirname + '/',
   plugins: __dirname + '/plugins/',
@@ -14,7 +14,8 @@ var dir = {
   dest: './build/'
 };
 
-  // modules
+// modules
+var express = require('express');
 var Metalsmith = require('metalsmith');
 var permalinks = require('metalsmith-permalinks');
 var inplace = require('metalsmith-in-place');
@@ -74,27 +75,11 @@ ms
   });
 
 if (!devBuild) {
-  var express = require('express');
   var app = express();
-
-  // set the port of our application
-  // process.env.PORT lets the port be set by Heroku
   var port = process.env.PORT || 8080;
-
-  // set the view engine to ejs
-  app.set('view engine', 'ejs');
-
   // make express look in the public directory for assets (css/js/img)
   app.use(express.static(__dirname + '/build'));
-
-  // set the home page route
-  app.get('/', function(req, res) {
-
-      // ejs render automatically looks in the views folder
-      res.render('index');
-  });
-
   app.listen(port, function() {
-      console.log('Our app is running on http://localhost:' + port);
+    console.log('Amsterdam running...');
   });
 }
